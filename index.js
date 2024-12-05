614089c4a3f634cc1a92c025aacd1763b6ce04e2
Eli Boninger <eboninger@gmail.com>
2024-12-05 15:39:57 -0500

    Fix authorziation header

--- index.js
+++ index.js
@@ -14,20 +14,31 @@ app.use(cors());
 app.get('/', (req, res) => res.send("ROOT GET"));
 
 app.get('/twitter-auth', async (req, res) => {
-    const response = await axios.get(`https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${process.env.X_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.X_REDIRECT_URI)}&scope=${scopes}&state=state&code_challenge=challenge&code_challenge_method=plain`, { headers: { Authorization: `Bearer ${credential}` } });
-    res.redirect(response.data);
+    try {
+        const response = await axios.get(`https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${process.env.X_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.X_REDIRECT_URI)}&scope=${scopes}&state=state&code_challenge=challenge&code_challenge_method=plain`, { headers: { Authorization: `Bearer ${credential}` } });
+        res.redirect(response.data);
+    } catch (e) {
+        console.log(e);
+    }
+
 });
 
 app.post('/token', async (req, res) => {
-    const { state, code } = req.body;
-    const params = new URLSearchParams();
-    params.append('code', code);
-    params.append('grant_type', 'authorization_code');
-    params.append('redirect_uri', process.env.X_REDIRECT_URI);
-    params.append('code_verifier', 'challenge');
-    const token = await axios.post('https://api.x.com/2/oauth2/token', params, { headers: { "Content-Type": "application/x-www-form-urlencoded", Authorization: `Bearer ${credential}` } });
-    console.log(res.data);
-    res.json(token);
+    try {
+        const { state, code } = req.body;
+        const params = new URLSearchParams();
+        params.append('code', code);
+        params.append('grant_type', 'authorization_code');
+        params.append('redirect_uri', process.env.X_REDIRECT_URI);
+        params.append('code_verifier', 'challenge');
+        const token = await axios.post('https://api.x.com/2/oauth2/token', params, { headers: { "Content-Type": "application/x-www-form-urlencoded", Authorization: `Basic ${credential}` } });
+        console.log(res.data);
+        res.json(token);
+
+    } catch (e) {
+        console.log(e);
+    }
+
 });
 
 app.listen(port, () => console.log("Server listening on port " + port));
