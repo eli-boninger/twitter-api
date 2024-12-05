dec8cf8f522f459ca3ae6590a4c39b1609a54624
Eli Boninger <eboninger@gmail.com>
2024-12-05 15:25:03 -0500

    add new endpoint to get oauth token

--- index.js
+++ index.js
@@ -13,14 +13,20 @@ app.use(cors());
 app.get('/', (req, res) => res.send("ROOT GET"));
 
 app.get('/twitter-auth', async (req, res) => {
-    const re = await axios.get(`https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${process.env.X_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.X_REDIRECT_URI)}&scope=${scopes}&state=state&code_challenge=challenge&code_challenge_method=plain`, { headers: { Authorization: `Bearer ${credential}` } });
-    res.sendStatus(200);
+    const response = await axios.get(`https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${process.env.X_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.X_REDIRECT_URI)}&scope=${scopes}&state=state&code_challenge=challenge&code_challenge_method=plain`, { headers: { Authorization: `Bearer ${credential}` } });
+    res.redirect(response.data);
 });
 
-app.get('/callback', (req, res) => {
-    const { state, code } = req.params;
-    console.log("STATE", state, "CODE", code);
-    res.sendStatus(200);
+app.post('/token', async (req, res) => {
+    const { state, code } = req.body;
+    const params = new URLSearchParams();
+    params.append('code', code);
+    params.append('grant_type', 'authorization_code');
+    params.append('redirect_uri', import.meta.env.VITE_CALLBACK_URI);
+    params.append('code_verifier', 'challenge');
+    const token = await axios.post('https://api.x.com/2/oauth2/token', params, { headers: { "Content-Type": "application/x-www-form-urlencoded", Authorization: `Bearer ${credential}` } });
+    console.log(res.data);
+    res.json(token);
 });
 
 app.listen(port, () => console.log("Server listening on port " + port));
